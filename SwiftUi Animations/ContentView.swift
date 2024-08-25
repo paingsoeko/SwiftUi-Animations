014fe6c0cc407212f46dac92d8420130d2c1c784
paingsoeko <social.kopaing@gmail.com>
2024-08-25 14:52:03 +0700

    Initial Commit

---
+++ SwiftUi Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUi Animations
+//
+//  Created by Paing Soe Ko on 25/8/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUi Animations/SwiftUi_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_AnimationsApp.swift
+//  SwiftUi Animations
+//
+//  Created by Paing Soe Ko on 25/8/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
